8ae9c6e5550145b64ad82fb0f138cf50eb57eca7
Kanaad Parvate <kanaadp@gmail.com>
2016-05-25 11:27:29 -0400

    removed printing headers

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.post('/test', function(request, response) {
   console.log(request.url);
       response.statusCode = 200;
     response.setHeader('Content-Type', 'text/html');
-  response.sendFile("pages/index.html")
+  response.render("pages/index");
 });
 
 app.listen(app.get('port'), function() {
