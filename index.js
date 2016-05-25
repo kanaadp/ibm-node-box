52d2599285a9a22db7d3883f0623b24fbd25c9ec
Kanaad Parvate <kanaadp@gmail.com>
2016-05-25 13:45:24 -0400

    trying post

--- index.js
+++ index.js
@@ -30,6 +30,22 @@ app.get('/test', function(request, response) {
   response.render("pages/index");
 });
 
+app.post('/fire', function(request, response) {
+  console.log("hi!");
+  console.log(request.method);
+  console.log(request.url);
+  response.statusCode = 200;
+  response.render("pages/index");
+});
+
+app.get('/fire', function(request, response) {
+  console.log("hello!");
+  console.log(request.method);
+  console.log(request.url);
+  response.statusCode = 200;
+  response.render("pages/index");
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
